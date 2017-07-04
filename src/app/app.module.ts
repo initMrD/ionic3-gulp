d503f8d289cf48f13ebbe4b6c6afc59a0e28481e
initmrd <initmrd@gmail.com>
2017-07-04 16:13:57 +0800

    添加懒加载的跳转示例

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {MyApp} from "./app.component";
 
 @NgModule({
   declarations: [
